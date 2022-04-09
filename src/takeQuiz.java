8470c47b9767bb10bffc9b25e21e87e2dfd47667
shrutisss-21 <94993960+shrutisss-21@users.noreply.github.com>
2022-04-08 21:46:20 -0400

    Add files via upload

--- src/takeQuiz.java
+++ src/takeQuiz.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
+import javax.swing.*;
+
 
 public class takeQuiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // use arraylist string so teachers can add incrementally increasing questions that students have to answer
         String q1 = "whatever question" + "\n(a)answer a\n (b) b \n (c) c \n (d)d";
         String q2 = "whatever second question" + "\n(a)answer a \n (b) b \n (c) c \n(d)d;";
+        String q3 = "whatever third question" + "\n(a)answer a \n (b) b \n (c) c \n(d)d;";
         // so on..
         // find a way to make it, so it interacts with teacher to change the question
 
 
         Question[] questions = {
-                new Question(q1, "a"), // question that corresponds to the qurrect answer
-                new Question(q2, "b")
+                new Question(q1, "a"), // question that corresponds to the correct answer
+                new Question(q2, "b"),
+                new Question(q3, "c")
         };
         Test(questions);
     }
@@ -28,6 +33,24 @@ public class takeQuiz {
         }
         System.out.println("You got " + score + "/" + questions.length);
     }
+
+    public class DropDown {
+        public static void DropDown(String[] args) {
+            String[] optionsToChoose = {"option 1", "option 2", "option 3", "option 4", "None of the options listed"};
+
+            String getFavOption = (String) JOptionPane.showInputDialog(
+                    null,
+                    "input from teacher",
+                    "Choose Option that best fits",
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    optionsToChoose,
+                    optionsToChoose[3]);
+
+            System.out.println("Your chosen option: " + getFavOption);
+        }
+    }
+
 }
 
 
