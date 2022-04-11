b85ef8c8bc5682eb6a58c1a57dddc91ea8761451
zonglin133 <102833970+zonglin133@users.noreply.github.com>
2022-04-11 14:23:23 -0400

    Add files via upload
    Please check campuswire for the image that might be helpful.
    Here are my posts on the campuswire:
    
    I have just updated the Grading.java and the OptionList.java for the students to upload any files of answer.
    
    See submission only works right now when the student is checking the submission right after he or she submitted the file.
    
    It needs the old version of CreateAccount.java to run and you might need to paste the users' information into AccountsData.txt and change the CreateAccount.java to the updated version to let the teacher see the file submission without cleanning the users' data.
    
    Manually grading function is not added yet. I would try more on that after I came back from my next course. However, as soon as the CreateAccount.java can work as intended without cleanning users' data each time(this occurs in the old version) or unable to create an account(this opccurs in the latest version), the updated OptionList.java should work out fine.

---
+++ src/Grading.java
@@ -0,0 +1,200 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Grading
+ *
+ * Used to grade the quizzes
+ *
+ * @author Zonglin Jia, L15
+ *
+ * @version 4/11/2022
+ *
+ */
+
+
+public class Grading {
+    //public boolean[] isCorrect;
+    public ArrayList<Questions> questions;
+    public ArrayList<String> answer;
+    public ArrayList<Integer> points;
+    public ArrayList<String> gradeAnswer;
+    public double pointsEarned;
+    public double pointsTotal;
+
+    public Grading(ArrayList<Questions> questions, ArrayList<String> answer, ArrayList<Integer> points) {
+        this.questions = questions;
+        this.answer = answer;
+        this.points = points;
+        pointsEarned = 0;
+        pointsTotal = 0;
+    }
+
+
+
+    public void setAnswer(ArrayList<String> answer) {
+        this.answer = answer;
+    }
+
+    public void setPointsEarned(double grade) {
+        this.pointsEarned = grade;
+    }
+
+    public void setQuiz(ArrayList<Questions> quiz) {
+        this.questions = questions;
+    }
+
+    //public void setIsCorrect
+
+    public ArrayList<Questions> getQuiz() {
+        return questions;
+    }
+
+    public ArrayList<String> getAnswer() {
+        return answer;
+    }
+
+    public double getPointsEarned() {
+        return pointsEarned;
+    }
+
+
+    public ArrayList<String> gradeAnswer(String studentFileName, String quizName, String stuName) {
+
+
+        //read the correct answer from the input fileName of quiz
+        //while the OptionList give the constructor correct answers and questions and points
+        //this readFile method could be omitted
+        //read the student's answer from the input fileName
+
+        ArrayList<String> submission = new ArrayList<String>();
+        submission.add(quizName);
+
+        ArrayList<String> list = new ArrayList<String>();
+        int g = 0;
+        int temp1 = 0;
+        int temp2 = 0;
+
+        try {
+            File fi = new File(studentFileName);
+            fi.createNewFile();
+            if (fi.exists()) {
+                BufferedReader br = new BufferedReader(new FileReader(fi));
+
+                String line = br.readLine();
+                while (line != null) {
+                    list.add(line);
+                    line = br.readLine();
+                    g++;
+                }
+
+                int c = 0;
+                int d = 0;
+                int f = 0;
+
+                for (int j = 0; j < g; j++) {
+                    if (list.get(j).contains(quizName)) {
+
+                        f = j;
+
+                    }
+                }
+
+                //System.out.println("test of list's start = " + list.get(f));
+
+                for (int i = 0; i < questions.size(); i++) {
+
+                    if (f == 0) {
+                        temp2 = 1;
+                    } else {
+                        temp1 = i + 1;
+                        temp2 = temp1 + f;
+                    }
+
+                    String stuTempAns = list.get(temp2);
+
+                    pointsTotal += points.get(i);
+
+                    if (answer.get(i).equals(stuTempAns)) {
+                        c++;
+                        pointsEarned += points.get(i);
+
+                        //System.out.println("correct answer count: " + c);
+                    }
+
+                    d = i + 1;
+                    //System.out.println("test of loop num " + d);
+                    //System.out.println();
+
+                    if (stuTempAns.indexOf("/") >= 0) {
+
+                        //int count = 0;
+                        String replace = stuTempAns;
+                        System.out.println("the whole String: " + stuTempAns);
+
+
+                        int count = 0;
+
+                        for(char y : stuTempAns.toCharArray()){
+                            if(y == '/'){
+                                count++;
+                            }
+                        }
+
+                        System.out.println("count: " + count);
+
+                        for (int l = 0; l < count; l++) {
+
+                            int temp = replace.indexOf("/");
+                            String upload = replace.substring(0, temp);
+
+                            if (l == 0) {
+                                //System.out.println("upload: " + upload);
+                                submission.add("The student's answer: ");
+                                submission.add(upload);
+
+                                replace = stuTempAns.substring(temp + 1);
+                                //System.out.println("replace: " + replace);
+                            } else {
+                                //System.out.println("upload: " + upload);
+                                submission.add(upload);
+
+                                temp = replace.indexOf("/");
+
+                                replace = replace.substring(temp + 1);
+                                //System.out.println("replace: " + replace);
+
+                            }
+
+                        }
+
+                    } else {
+                        submission.add("The student's answer: " + stuTempAns);
+                    }
+
+                    submission.add("The correct answer: " + answer.get(i));
+
+                }
+
+
+                String percent = "%";
+                String score = String.format("Student " + stuName + "'s grade: %.2f", (pointsEarned/pointsTotal) * 100);
+                String combo = score + percent;
+                submission.add(combo);
+
+
+                br.close();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return submission;
+    }
+
+
+}
