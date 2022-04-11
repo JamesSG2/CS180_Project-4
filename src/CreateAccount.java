be795a578989a805bfc9c1d438c4dbace88798bb
zonglin133 <102833970+zonglin133@users.noreply.github.com>
2022-04-11 17:57:40 -0400

    Add files via upload

--- src/CreateAccount.java
+++ src/CreateAccount.java
@@ -49,7 +49,6 @@ public class CreateAccount {
     private void readAccountsDataFile() throws IOException {
         File f = new File("AccountsData.txt");
         BufferedReader br = new BufferedReader(new FileReader(f));
-        accountsData = new ArrayList<>();
         String line = br.readLine();
         while (line != null) {
             accountsData.add(line);
@@ -61,11 +60,6 @@ public class CreateAccount {
     private void writeAccountsDataFile() throws IOException {
         File f = new File("AccountsData.txt");
         PrintWriter writer = new PrintWriter(f);
-
-        readAccountsDataFile();
-        for (String line : accountsData) {
-            writer.append(null);
-        }
 
         for (String line : accountsData) {
             writer.println(line);
