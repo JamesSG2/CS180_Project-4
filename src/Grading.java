71efd62b4a75693dbfbca1a7e31d3c33fdce75dd
zonglin133 <102833970+zonglin133@users.noreply.github.com>
2022-04-21 16:32:28 -0400

    Add files via upload
    Updated Grading.java, same as the one I sent in Tuesday to Debugging and Server-Client branch

--- src/Grading.java
+++ src/Grading.java
@@ -18,22 +18,23 @@ import java.util.ArrayList;
 public class Grading {
     //public boolean[] isCorrect;
     public ArrayList<Questions> questions;
-    public ArrayList<String> answer;
+    public ArrayList<String> correctAnswer;
+    //ArrayList<String> studentsAnswer;
     public ArrayList<Integer> points;
     public ArrayList<String> gradeAnswer;
     public double pointsEarned;
     public double pointsTotal;
 
-    public Grading(ArrayList<Questions> questions, ArrayList<String> answer, ArrayList<Integer> points) {
+    public Grading(ArrayList<Questions> questions, ArrayList<String> correctAnswer, ArrayList<Integer> points) {
         this.questions = questions;
-        this.answer = answer;
+        this.correctAnswer = correctAnswer;
         this.points = points;
         pointsEarned = 0;
         pointsTotal = 0;
     }
 
-    public void setAnswer(ArrayList<String> answer) {
-        this.answer = answer;
+    public void setCorrectAnswerAnswer(ArrayList<String> answer) {
+        this.correctAnswer = answer;
     }
 
     public void setPointsEarned(double grade) {
@@ -42,6 +43,10 @@ public class Grading {
 
     public void setPointsTotal(double grade) {
         this.pointsTotal = grade;
+    }
+
+    public void setCorrectAnswer(ArrayList<String> correctAnswer) {
+        this.correctAnswer = correctAnswer;
     }
 
     public void setQuiz(ArrayList<Questions> quiz) {
@@ -53,14 +58,15 @@ public class Grading {
     }
 
     public ArrayList<String> getAnswer() {
-        return answer;
+        return correctAnswer;
     }
 
     public double getPointsEarned() {
         return pointsEarned;
     }
 
-    public ArrayList<String> gradeAnswer(String studentFileName, String quizName, String stuName) {
+
+    public ArrayList<String> autoGrade (ArrayList<String> studentsAnswer, String quizName, String stuName) {
 
         //read the correct answer from the input fileName of quiz
         //read the student's answer from the input fileName
@@ -75,114 +81,66 @@ public class Grading {
         Timestamp timestamp = new Timestamp(datetime);
         submission.add("Time Stamp: " + timestamp);
 
-        ArrayList<String> list = new ArrayList<String>();
-        int g = 0;
-        int temp1 = 0;
-        int temp2 = 0;
+        int countOfRightAnswer = 0;
 
-        try {
-            File fi = new File(studentFileName);
+        for (int i = 0; i < questions.size(); i++) {
 
-            if (fi.exists()) {
-                BufferedReader br = new BufferedReader(new FileReader(fi));
+            String stuTempAns = studentsAnswer.get(i);
 
-                String line = br.readLine();
-                while (line != null) {
-                    list.add(line);
-                    line = br.readLine();
-                    g++;
-                }
+            pointsTotal += points.get(i);
 
-                int c = 0;
-                int d = 0;
-                int f = 0;
+            if (correctAnswer.get(i).equals(stuTempAns)) {
+                countOfRightAnswer++;
+                pointsEarned += points.get(i);
+            }
 
-                for (int j = 0; j < g; j++) {
-                    if (list.get(j).contains(quizName)) {
-                        f = j;
-                        break;
+            int qNum = i + 1;
+            submission.add("Question #" + qNum + ":");
+
+            if (stuTempAns.indexOf("/") >= 0) {
+
+                String replace = stuTempAns;
+                int countsOfLine = 0;
+
+                for (char y : stuTempAns.toCharArray()) {
+                    if (y == '/') {
+                        countsOfLine++;
                     }
                 }
 
-                for (int i = 0; i < questions.size(); i++) {
+                for (int l = 0; l < countsOfLine; l++) {
 
-                    if (f == 0) {
-                        temp2 = 1;
-                    } else {
-                        temp1 = i + 1;
-                        temp2 = temp1 + f;
-                    }
+                    int temp = replace.indexOf("/");
+                    String upload = replace.substring(0, temp);
 
-                    String stuTempAns = list.get(temp2);
+                    if (l == 0) {
+                        submission.add("The student's answer: ");
+                        submission.add(upload);
 
-                    pointsTotal += points.get(i);
-
-                    if (answer.get(i).equals(stuTempAns)) {
-                        c++;
-                        pointsEarned += points.get(i);
-                    }
-
-                    int qNum = i + 1;
-                    submission.add("Question #" + qNum + ":");
-
-                    if (stuTempAns.indexOf("/") >= 0) {
-
-                        String replace = stuTempAns;
-
-                        int count = 0;
-
-                        for (char y : stuTempAns.toCharArray()) {
-                            if (y == '/') {
-                                count++;
-                            }
-                        }
-
-                        for (int l = 0; l < count; l++) {
-
-                            int temp = replace.indexOf("/");
-                            String upload = replace.substring(0, temp);
-
-                            if (l == 0) {
-                                submission.add("The student's answer: ");
-                                submission.add(upload);
-
-                                replace = stuTempAns.substring(temp + 1);
-
-                            } else {
-
-                                submission.add(upload);
-
-                                temp = replace.indexOf("/");
-
-                                replace = replace.substring(temp + 1);
-
-                            }
-
-                        }
+                        replace = stuTempAns.substring(temp + 1);
 
                     } else {
-                        submission.add("The student's answer: " + stuTempAns);
+                        submission.add(upload);
+                        temp = replace.indexOf("/");
+                        replace = replace.substring(temp + 1);
                     }
-
-                    submission.add("The correct answer: " + answer.get(i));
-                    submission.add("Point Value: " + points.get(i));
                 }
-
-                String percent = "%";
-                String score = String.format("Student " + stuName + "'s grade: %.2f",
-                        (pointsEarned / pointsTotal) * 100);
-                String combo = score + percent;
-
-                submission.add(combo);
-
-                br.close();
 
             } else {
-                System.out.println("Error! File Not Found.");
+                submission.add("The student's answer: " + stuTempAns);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            submission.add("The correct answer: " + correctAnswer.get(i));
+            submission.add("Point Value: " + points.get(i));
+
         }
+
+        String percent = "%";
+        String score = String.format("Student " + stuName + "'s grade: %.2f",
+                (pointsEarned / pointsTotal) * 100);
+        String combo = score + percent;
+
+        submission.add(combo);
 
         return submission;
     }
