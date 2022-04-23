9adc3e3be5e5c4c41294465baa57872577fd5255
shrutisss-21 <94993960+shrutisss-21@users.noreply.github.com>
2022-04-23 16:45:24 -0400

    Add files via upload

--- src/MultChoice.java
+++ src/MultChoice.java
@@ -1,3 +1,301 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class MultChoice implements ActionListener{
+
+    String[] questions = 	{
+            // in reality it would be ( "Question 1:" + string variable from option list that has the question)
+            "Question 1",
+            "Question 2",
+            "Question 3",
+            "Question 4"
+    };
+    String[][] options = 	{
+            {"Option 1","Option 2","Option 3","Option 4"},
+            {"Option 1","Option 2","Option 3","Option 4"},
+            {"Option 1","Option 2","Option 3","Option 4"},
+            {"Option 1","Option 2","Option 3","Option 4"}
+    };
+    char[] answers = 		{
+            'A',
+            'B',
+            'C',
+            'C'
+    };
+    char guess;
+    char answer;
+    int index;
+    int correct_guesses =0;
+    int total_questions = questions.length;
+    int result;
+    int seconds=10;
+
+    JFrame frame = new JFrame();
+    JTextField textfield = new JTextField();
+    JTextArea textarea = new JTextArea();
+    JButton buttonA = new JButton();
+    JButton buttonB = new JButton();
+    JButton buttonC = new JButton();
+    JButton buttonD = new JButton();
+    JLabel answer_labelA = new JLabel();
+    JLabel answer_labelB = new JLabel();
+    JLabel answer_labelC = new JLabel();
+    JLabel answer_labelD = new JLabel();
+    JLabel time_label = new JLabel();
+    JLabel seconds_left = new JLabel();
+    JTextField number_right = new JTextField();
+    JTextField percentage = new JTextField();
+
+    Timer timer = new Timer(1000, new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            seconds--;
+            seconds_left.setText(String.valueOf(seconds));
+            if(seconds<=0) {
+                displayAnswer();
+            }
+        }
+    });
+
+    public MultChoice() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(650,650);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(null);
+        frame.setResizable(false);
+
+        textfield.setBounds(0,0,650,50);
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(255,255,255));
+        textfield.setFont(new Font("Arial",Font.BOLD,30));
+        textfield.setBorder(BorderFactory.createBevelBorder(1));
+        textfield.setHorizontalAlignment(JTextField.CENTER);
+        textfield.setEditable(false);
+
+        textarea.setBounds(0,50,650,50);
+        textarea.setLineWrap(true);
+        textarea.setWrapStyleWord(true);
+        textarea.setBackground(new Color(25,25,25));
+        textarea.setForeground(new Color(255,255,255));
+        textarea.setFont(new Font("Arial",Font.BOLD,25));
+        textarea.setBorder(BorderFactory.createBevelBorder(1));
+        textarea.setEditable(false);
+
+        buttonA.setBounds(0,100,100,100);
+        buttonA.setFont(new Font("Arial",Font.BOLD,20));
+        buttonA.setFocusable(false);
+        buttonA.addActionListener(this);
+        buttonA.setText("A");
+
+        buttonB.setBounds(0,200,100,100);
+        buttonB.setFont(new Font("Arial",Font.BOLD,20));
+        buttonB.setFocusable(false);
+        buttonB.addActionListener(this);
+        buttonB.setText("B");
+
+        buttonC.setBounds(0,300,100,100);
+        buttonC.setFont(new Font("Arial",Font.BOLD,20));
+        buttonC.setFocusable(false);
+        buttonC.addActionListener(this);
+        buttonC.setText("C");
+
+        buttonD.setBounds(0,400,100,100);
+        buttonD.setFont(new Font("Arial",Font.BOLD,20));
+        buttonD.setFocusable(false);
+        buttonD.addActionListener(this);
+        buttonD.setText("D");
+
+        answer_labelA.setBounds(125,100,500,100);
+        answer_labelA.setBackground(new Color(50,50,20));
+        answer_labelA.setForeground(new Color(25,255,0));
+        answer_labelA.setFont(new Font("Arial",Font.PLAIN,35));
+
+        answer_labelB.setBounds(125,200,500,100);
+        answer_labelB.setBackground(new Color(50,50,20));
+        answer_labelB.setForeground(new Color(25,255,0));
+        answer_labelB.setFont(new Font("Arial",Font.PLAIN,35));
+
+        answer_labelC.setBounds(125,300,500,100);
+        answer_labelC.setBackground(new Color(50,50,20));
+        answer_labelC.setForeground(new Color(25,255,0));
+        answer_labelC.setFont(new Font("Arial",Font.PLAIN,35));
+
+        answer_labelD.setBounds(125,400,500,100);
+        answer_labelD.setBackground(new Color(50,50,20));
+        answer_labelD.setForeground(new Color(25,255,0));
+        answer_labelD.setFont(new Font("Arial",Font.PLAIN,35));
+
+        seconds_left.setBounds(535,510,100,100);
+        seconds_left.setBackground(new Color(25,25,20));
+        seconds_left.setForeground(new Color(255,0,0));
+        seconds_left.setFont(new Font("Arial",Font.BOLD,60));
+        seconds_left.setBorder(BorderFactory.createBevelBorder(1));
+        seconds_left.setOpaque(true);
+        seconds_left.setHorizontalAlignment(JTextField.CENTER);
+        seconds_left.setText(String.valueOf(seconds));
+
+        time_label.setBounds(535,475,100,25);
+        time_label.setBackground(new Color(50,50,20));
+        time_label.setForeground(new Color(25,255,255));
+        time_label.setFont(new Font("Arial",Font.PLAIN,16));
+        time_label.setHorizontalAlignment(JTextField.CENTER);
+        time_label.setText("timer");
+
+        number_right.setBounds(225,225,200,100);
+        number_right.setBackground(new Color(25,25,20));
+        number_right.setForeground(new Color(25,255,0));
+        number_right.setFont(new Font("Arial",Font.BOLD,50));
+        number_right.setBorder(BorderFactory.createBevelBorder(1));
+        number_right.setHorizontalAlignment(JTextField.CENTER);
+        number_right.setEditable(false);
+
+        percentage.setBounds(225,325,200,100);
+        percentage.setBackground(new Color(25,25,20));
+        percentage.setForeground(new Color(25,255,0));
+        percentage.setFont(new Font("Arial",Font.BOLD,50));
+        percentage.setBorder(BorderFactory.createBevelBorder(1));
+        percentage.setHorizontalAlignment(JTextField.CENTER);
+        percentage.setEditable(false);
+
+        frame.add(time_label);
+        frame.add(seconds_left);
+        frame.add(answer_labelA);
+        frame.add(answer_labelB);
+        frame.add(answer_labelC);
+        frame.add(answer_labelD);
+        frame.add(buttonA);
+        frame.add(buttonB);
+        frame.add(buttonC);
+        frame.add(buttonD);
+        frame.add(textarea);
+        frame.add(textfield);
+        frame.setVisible(true);
+
+        nextQuestion();
+    }
+    public void nextQuestion() {
+
+        if(index>=total_questions) {
+            results();
+        }
+        else {
+            textfield.setText("Question "+(index+1));
+            textarea.setText(questions[index]);
+            answer_labelA.setText(options[index][0]);
+            answer_labelB.setText(options[index][1]);
+            answer_labelC.setText(options[index][2]);
+            answer_labelD.setText(options[index][3]);
+            timer.start();
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        buttonA.setEnabled(false);
+        buttonB.setEnabled(false);
+        buttonC.setEnabled(false);
+        buttonD.setEnabled(false);
+
+        if(e.getSource()==buttonA) {
+            answer= 'A';
+            if(answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+        if(e.getSource()==buttonB) {
+            answer= 'B';
+            if(answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+        if(e.getSource()==buttonC) {
+            answer= 'C';
+            if(answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+        if(e.getSource()==buttonD) {
+            answer= 'D';
+            if(answer == answers[index]) {
+                correct_guesses++;
+            }
+        }
+        displayAnswer();
+    }
+    public void displayAnswer() {
+
+        timer.stop();
+
+        buttonA.setEnabled(false);
+        buttonB.setEnabled(false);
+        buttonC.setEnabled(false);
+        buttonD.setEnabled(false);
+
+        if(answers[index] != 'A')
+            answer_labelA.setForeground(new Color(255,0,0));
+        if(answers[index] != 'B')
+            answer_labelB.setForeground(new Color(255,0,0));
+        if(answers[index] != 'C')
+            answer_labelC.setForeground(new Color(255,0,0));
+        if(answers[index] != 'D')
+            answer_labelD.setForeground(new Color(255,0,0));
+
+
+
+        Timer pause = new Timer(2000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                answer_labelA.setForeground(new Color(25,255,0));
+                answer_labelB.setForeground(new Color(25,255,0));
+                answer_labelC.setForeground(new Color(25,255,0));
+                answer_labelD.setForeground(new Color(25,255,0));
+
+                answer = ' ';
+                seconds=10;
+                seconds_left.setText(String.valueOf(seconds));
+                buttonA.setEnabled(true);
+                buttonB.setEnabled(true);
+                buttonC.setEnabled(true);
+                buttonD.setEnabled(true);
+                index++;
+                nextQuestion();
+            }
+        });
+        pause.setRepeats(false);
+        pause.start();
+    }
+    public void results(){
+
+        buttonA.setEnabled(false);
+        buttonB.setEnabled(false);
+        buttonC.setEnabled(false);
+        buttonD.setEnabled(false);
+
+        result = (int)((correct_guesses/(double)total_questions)*100);
+
+        textfield.setText("RESULTS!");
+        textarea.setText("");
+        answer_labelA.setText("");
+        answer_labelB.setText("");
+        answer_labelC.setText("");
+        answer_labelD.setText("");
+
+        number_right.setText("("+correct_guesses+"/"+total_questions+")");
+        percentage.setText(result+"%");
+
+        frame.add(number_right);
+        frame.add(percentage);
+
+    }
+}
+
+
+
+/*
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.ButtonGroup;
@@ -5,6 +303,9 @@ import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+
+shruti
+
 
 public class MultChoice extends JFrame implements ActionListener {
     JButton button, buttonTwo, buttonThree, buttonFour, bton;
@@ -25,25 +326,25 @@ public class MultChoice extends JFrame implements ActionListener {
         //title.setBorder(new EmptyBoarder(0,10,0,0));
 
         label = new JLabel ("Question 1: Do you believe that you are hot ");
-        label.setBounds (300,100,360,50);
+        label.setBounds (280,100,360,50);
         //title setFont(new Font ("Times New Roman", Font.BOLD, 28));
         //label.setBorder(new EmptyBoarder(0,10,0,0));
 
         bton = new JButton ("Check Answer");
-        bton.setBounds (250,420,250,50);
+        bton.setBounds (300,420,250,50);
 
 
         button = new JButton("Option 1");
-        button.setBounds (300,200,300,50);
+        button.setBounds (270,200,300,50);
 
         buttonTwo = new JButton("Option 2");
-        buttonTwo.setBounds(300,250,300,50);
+        buttonTwo.setBounds(270,250,300,50);
 
         buttonThree = new JButton ("Option 3");
-        buttonThree.setBounds(300,300,300,50);
+        buttonThree.setBounds(270,300,300,50);
 
         buttonFour = new JButton ("Option 3");
-        buttonFour.setBounds(300,350,300,50);
+        buttonFour.setBounds(270,350,300,50);
 
         ButtonGroup bGroup = new ButtonGroup();
         bGroup.add(button);
@@ -85,3 +386,5 @@ public class MultChoice extends JFrame implements ActionListener {
 
     }
 }
+
+ */
--- src/Runnable.java
+++ src/Runnable.java
@@ -1,9 +1,5 @@
-/**
- * runnable class for multiple choice GUI
- * Shruti 
- */
 public class Runnable {
     public static void main(String[] args) {
-        MultChoice frame = new MultChoice();
+        LoginPage frame = new LoginPage();
     }
 }
