48e11b43da1cb0b8e1bbd660db1115d6520f6892
shrutisss-21 <94993960+shrutisss-21@users.noreply.github.com>
2022-05-01 19:16:03 -0400

    fixed an error back

--- src/Client.java
+++ src/Client.java
@@ -676,7 +676,7 @@ public class Client implements Serializable {
 
 
         // TODO: All codes below are for the student to input
-        // Shruti input
+        // Shruti's addition
         boolean student = true;
 
         if (userType.equalsIgnoreCase("Student")) {
